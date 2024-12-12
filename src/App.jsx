94275527fd165ff52baf1ca14ae603fd80f8f2c0
Fadhil Akbar Djunaedi <fadhilakbar57894@gmail.com>
2024-12-12 17:01:55 +0700

    push kodeinclub landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Navbar from "./components/Navbar";
